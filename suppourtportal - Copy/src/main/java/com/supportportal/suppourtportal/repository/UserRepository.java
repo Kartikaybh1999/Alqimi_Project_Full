4a45fdd78f1fe6e5bf4c4d5eeed89f9c3c10e28f
Kartikaybh1999 <kartikaybh99@gmail.com>
2022-06-06 15:44:02 +0530

    update

---
+++ suppourtportal - Copy/src/main/java/com/supportportal/suppourtportal/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.supportportal.suppourtportal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.supportportal.suppourtportal.domain.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findUserByUsername(String username);
+
+    User findUserByEmail(String email);
+}
